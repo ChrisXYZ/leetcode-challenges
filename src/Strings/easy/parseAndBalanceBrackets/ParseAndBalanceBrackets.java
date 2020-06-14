28bf501aac42bc593dda7b3a1e509633b7e56f55
ChrisXYZ <christopherto@hotmail.co.uk>
2020-06-14 15:37:07 +0100

    Completing "ParseAndBalanceBrackets" challenge (hired)

---
+++ src/Strings/easy/parseAndBalanceBrackets/ParseAndBalanceBrackets.java
@@ -0,0 +1,29 @@
+package Strings.easy.parseAndBalanceBrackets;
+
+public class ParseAndBalanceBrackets {
+
+    public static void main(String[] args) {
+        String input = "><";
+        System.out.println(solution(input));
+
+    }
+
+
+    public static String solution(String angles) {
+        String appendToEnd = "";
+        String appendToStart = "";
+        for (int i = 0; i < angles.length(); i++) {
+            if (angles.charAt(i) == '>') {
+                if (appendToEnd.length() == 0) {
+                    appendToStart = appendToStart + "<";
+                } else {
+                    appendToEnd = appendToEnd.substring(0, appendToEnd.length() - 1);
+                }
+            } else {
+                appendToEnd = appendToEnd + ">";
+            }
+
+        }
+        return (appendToStart + angles + appendToEnd);
+    }
+}
